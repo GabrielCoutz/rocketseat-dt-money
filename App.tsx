9cd46d74bd9a1f2adbfa1cfd06b307104bce48c0
Gabrielcoutz <gabriel.coutinho.cassiano@gmail.com>
2026-01-27 15:32:01 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import './global.css';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-gray-900">
+      <Text className="text-white">Hello World</Text>
+    </View>
+  );
+}
